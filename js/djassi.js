6dc427ae3114092092f17fdf8a7429dc683b0445
djassij <djassij@gmail.com>
2015-07-17 19:03:17 -0400

    Update djassi.js

--- js/djassi.js
+++ js/djassi.js
@@ -1,13 +1,13 @@
-function swingHover(#nav, swing){
+function swingHover('#nav', 'swing'){
     '#nav' = $('#nav');
     '#nav'.hover(
         function() {
-            '#nav'.addClass('animated ' + swing);        
+            '#nav'.addClass('animated ' + 'swing');        
         },
         function(){
             //wait for swing to finish before removing classes
             window.setTimeout( function(){
-                '#nav'.removeClass('animated ' + swing);
+                '#nav'.removeClass('animated ' + 'swing');
             }, 2000);         
         });
 }
